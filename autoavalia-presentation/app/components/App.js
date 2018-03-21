f94804d00ed4349810d64f579429aedcc963c7a8
lsilva <lsilva@webmotors.com.br>
2018-03-21 11:16:32 -0300

    change - eslint and empty components

--- autoavalia-presentation/app/components/App.js
+++ autoavalia-presentation/app/components/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import img from '../assets/images/react_logo_512x512.png';
+import Top from './Header/index';
+import Routes from './Routes/index';
+import Footer from './Footer/index';
 
 const App = () => {
   return (
-    <div>
-      <h2 id="heading">Hello ReactJS</h2>
-      <img
-        className="image"
-        style={{ margin: '0.5em' }}
-        height="40"
-        width="40"
-        src={img}
-        alt="React Logo"
-      />
-    </div>
+  <div>
+    <Top />
+    <Routes />
+    <Footer />
+  </div>
   );
 };
 
--- autoavalia-presentation/app/main.js
+++ autoavalia-presentation/app/main.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import { render } from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import App from './components/App';
 
-import Root from './config/Root';
-
-const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('root'),
-  );
-};
-
-render(Root);
-
-if (module.hot) {
-  module.hot.accept('./config/Root', () => {
-    const newApp = require('./config/Root').default;
-    render(newApp);
-  });
-}
+render(
+  (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  ), document.getElementById('root')
+);
