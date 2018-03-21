7f72251bc062cfbbaa3fb3fd8e640743dcec37c9
AlbaniV <albani.tecnologia@webmotors.com.br>
2018-03-21 10:11:57 -0300

    ADD API

---
+++ autoavalia-api/AutoAvalia-API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AutoAvalia_API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
