881781849ff923d78f13312a3c7d006cca977c8c
Felipe Assunção <felipeassuncao.net@gmail.com>
2018-03-23 20:24:56 -0300

    x

---
+++ autoavalia-api/AutoAvalia-API/Controllers/AssetsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Amazon;
+using Webmotors.Shared.IO;
+
+namespace Webmotors.Api.Controllers
+{
+    public class AssetsController : ApiController
+    {
+        private IFileUploadClient Client { get; }
+
+        public AssetsController()
+        {
+            Client = new AmazonFileUploadClient("", "");
+        }
+
+        public void UploadImage()
+        {
+
+        }
+    }
+}
