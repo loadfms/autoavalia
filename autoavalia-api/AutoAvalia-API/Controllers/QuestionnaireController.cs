53a3be81b3a4518375d401be3fa87aa446d71ad5
Felipe Assunção <felipeassuncao.net@gmail.com>
2018-03-23 22:48:34 -0300

    chnages

--- autoavalia-api/AutoAvalia-API/Controllers/QuestionnaireController.cs
+++ autoavalia-api/AutoAvalia-API/Controllers/QuestionnaireController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Webmotors.Api.Classes;
+﻿using Webmotors.Api.Classes;
 
 namespace Webmotors.Api.Controllers
 {
