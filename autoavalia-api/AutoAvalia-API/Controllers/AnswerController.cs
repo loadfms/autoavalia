cb5801ec0240c66a520049ca4df91293fd5db653
AlbaniV <albani.tecnologia@webmotors.com.br>
2018-03-24 05:19:19 -0300

    commit

--- autoavalia-api/AutoAvalia-API/Controllers/AnswerController.cs
+++ autoavalia-api/AutoAvalia-API/Controllers/AnswerController.cs
@@ -1,9 +1,25 @@
-﻿using Webmotors.Api.Classes;
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using Webmotors.Api.Classes;
+using Webmotors.Shared.Database.NoSql;
 
 namespace Webmotors.Api.Controllers
 {
     public class AnswerController : CrudApi<Answer>
     {
+        [HttpGet]
+        [EnableCors(origins: "http://localhost:8080", headers: "*", methods: "*")]
+        [Route("api/answer/{questionnaireId}/{questionId}/{value}")]
+        public void Answer(string questionnaireId, string questionId, bool value)
+        {
+            var answerRepo = new QuickRepository<Answer>();
+            answerRepo.Add(new Answer
+            {
+                QuestionId = questionId,
+                QuestionnaireId = questionnaireId,
+                Value = value
+            });
+        }
 
     }
 }
