e8ec2f4a7da7582e1c76be8f744b7b38a755cf08
Felipe Assunção <felipeassuncao.net@gmail.com>
2018-03-24 01:05:13 -0300

    x

---
+++ autoavalia-api/Webmotors.Shared/Caching/CacheContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace Webmotors.Shared.Caching
+{
+    public sealed class CacheContainer
+    {
+        private static readonly Lazy<CacheContainer> lazy =
+            new Lazy<CacheContainer>(() => new CacheContainer());
+
+        public static CacheContainer Instance => lazy.Value;
+
+        public MemoryCache Cache { get; set; }
+
+        private CacheContainer()
+        {
+            
+        }
+
+        public void Initialize()
+        {
+            Cache = new MemoryCache("sharedCacheContainer");
+        }
+    }
+}
