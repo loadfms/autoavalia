265fbc6cfe841ae6d8e3cb1ae1d8052e3e807fe5
Felipe Assunção <felipeassuncao.net@gmail.com>
2018-03-23 22:18:54 -0300

    x

---
+++ autoavalia-api/AutoAvalia-API/Classes/Questionnaire.cs
@@ -0,0 +1,11 @@
+﻿using Webmotors.Shared.Database;
+
+namespace Webmotors.Api.Classes
+{
+    public class Questionnaire : IEntity<int>
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int AdvertiseId { get; set; }
+    }
+}
---
+++ autoavalia-api/AutoAvalia-API/Controllers/QuestionnaireController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Webmotors.Api.Classes;
+
+namespace Webmotors.Api.Controllers
+{
+    public class QuestionnaireController : CrudApi<Questionnaire, int>
+    {
+        public void Start(int userId, int advertiseId)
+        {
+
+        }
+    }
+}
